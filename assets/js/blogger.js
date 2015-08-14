d32aad0d18d94bf7d807a9f451cb4e5083be06b9
unknown <MTerrill_Li@dev5.bsecode.inc>
2015-08-14 11:19:01 -0500

    JSON file fix, JSON Valid

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,7 +1,11 @@
 $(document).ready(getblog);
 	function getblog(){
+		$.getJSON("entries.json", function(data){
+		$each(data.entries, function(key, value){
 		console.log("hello world")
-	}
+		})
+	})
+}
 function pager(){
 	
 	
